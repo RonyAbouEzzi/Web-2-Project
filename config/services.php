4c28c75448ab98e2d5f517f0c5f321a7f6056d34
Rony <Rony@Rony_ae.local>
2026-03-08 18:04:38 +0200

    Improve auth flows and require password setup after social login

--- config/services.php
+++ config/services.php
@@ -41,10 +41,10 @@ return [
         'redirect'      => env('GOOGLE_REDIRECT_URI', '/auth/google/callback'),
     ],
 
-    'facebook' => [
-        'client_id'     => env('FACEBOOK_CLIENT_ID'),
-        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect'      => env('FACEBOOK_REDIRECT_URI', '/auth/facebook/callback'),
+    'github' => [
+        'client_id'     => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect'      => env('GITHUB_REDIRECT_URI', '/auth/github/callback'),
     ],
 
 ];
